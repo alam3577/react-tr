c00151e52fdc004554b212a0b484ee2ddea55829
alam3577 <sajjadalam3577@gmail.com>
2021-10-30 18:56:13 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Data from "./component/Data";
 function App() {
+  const [state, setstate] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to react js</h1>
+      <button onClick={() => setstate(true)}>Click to get more data</button>
+      {state && <Data />}
     </div>
   );
 }
---
+++ src/component/Data.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Data() {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Hi Data is from component/Data</h1>
+    </div>
+  );
+}
+
+export default Data;
